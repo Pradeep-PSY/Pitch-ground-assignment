532f506889e92e980cbdfd8f557d29f4af8fad73
Danny12244 <pradeep.py725@gmail.com>
2022-11-11 14:07:41 +0530

    todo-app-ts

---
+++ ts-todo-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <ChakraProvider>
+   <Button ></Button>
+  </ChakraProvider>
+  );
+}
+
+export default App;
